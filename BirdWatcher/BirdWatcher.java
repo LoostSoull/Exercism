0a33c43af2dd9d6e93aa5172fa34d1ffe9e3e8d2
lucas silva <lucas.silva.cardoso1996@gmail.com>
2023-07-28 00:23:52 -0300

    tarefa não concluida

---
+++ BirdWatcher/BirdWatcher.java
@@ -0,0 +1,19 @@
+package br.com.BirdWatcher;
+
+public class BirdWatcher {
+	
+	public int[] birds;
+	
+	
+
+	
+
+
+	public static int[] getLastWeek() {
+		int[] lastWeek = {2,5,0,7,4,1};
+		return lastWeek;
+		
+
+	}
+
+}
