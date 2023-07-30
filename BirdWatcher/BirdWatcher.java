6b8ffd5ced6614e30e3af6e8fb022c5e76e984fb
lucas silva <lucas.silva.cardoso1996@gmail.com>
2023-07-30 03:03:35 -0300

    continuacao_birdwatcher

--- BirdWatcher/BirdWatcher.java
+++ BirdWatcher/BirdWatcher.java
@@ -1,19 +1,60 @@
 package br.com.BirdWatcher;
 
 public class BirdWatcher {
-	
-	public int[] birds;
-	
-	
 
-	
+	static int[] birds = { 2, 5, 0, 7, 4, 1 };
 
+	public static void getLastWeek() {
 
-	public static int[] getLastWeek() {
-		int[] lastWeek = {2,5,0,7,4,1};
-		return lastWeek;
-		
+		for (int v = 0; v < birds.length; v++) {
 
+			System.out.print(birds[v] + " , ");
+		}
+	}
+
+	public static void getToday() {
+		System.out.println(birds[5]);
+	}
+
+	public static void incrementTodaysCount() {
+
+		System.out.println(birds[0]);
+	}
+
+	public static void getCountForFirstDays() {
+
+		int sum = 0;
+		for (int i = 0; i < birds.length; i++) {
+
+			sum += birds[i];
+		}
+		System.out.println(sum);
+	}
+
+	public static void hasDayWithoutBirds() {
+
+		boolean res = true;
+		for (int v = 0; v < birds.length; v++) {
+
+			if (birds[v] == 0) {
+
+				res = true;
+				System.out.println(res);
+			}
+
+		}
+
+	}
+
+	public static void getBusyDays() {
+		int n = 0;
+		for (int c = 0; c < birds.length; c++) {
+			if (birds[c] >= 5) {
+				n = n + 1;
+			}
+
+		}
+		System.out.println(n);
 	}
 
 }
--- BirdWatcher/Program.java
+++ BirdWatcher/Program.java
@@ -4,10 +4,18 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		BirdWatcher.getLastWeek();
+
+		BirdWatcher.getToday();
+
+		BirdWatcher.incrementTodaysCount();
+
+		BirdWatcher.hasDayWithoutBirds();
 		
+		BirdWatcher.getCountForFirstDays();
 		
-		
-		System.out.println(BirdWatcher.getLastWeek());
+		BirdWatcher.getBusyDays();
 	}
 
 }
